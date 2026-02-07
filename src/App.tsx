ae4cefe8980eab7234d5408a07d2cbc934045db5
nikesh chauhdari <info.nikesh12@gmail.com>
2026-02-07 23:47:50 +0545

    rms

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Home from "./pages/Home"
 import SignUp from "./pages/SignUp"
+import SignIn from "./pages/SignIn"
+import { ToastContainer } from "react-toastify"
 
+
+export const myroute=createBrowserRouter([
+{path:"",Component:Home},
+{path:"home",Component:Home},
+{path:"signup",Component:SignUp},
+{path:"login",Component:SignIn}
+])
 const App = () => {
+
   return (
    <>
-   <SignUp/>
+   <RouterProvider router={myroute}/>
+  <ToastContainer/>
    </>
   )
 }
