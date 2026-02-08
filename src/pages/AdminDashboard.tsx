150cb316a94b7a92293cb92eae350913ad0ab281
nikesh chauhdari <info.nikesh12@gmail.com>
2026-02-08 22:06:15 +0545

    rms

---
+++ src/pages/AdminDashboard.tsx
@@ -0,0 +1,17 @@
+
+
+const AdminDashboard = () => {
+  return (
+    <>
+   <main>
+    <section>
+        <aside className="w-80 bg-[#102237] h-screen">
+
+        </aside>
+    </section>
+   </main>
+    </>
+  )
+}
+
+export default AdminDashboard
