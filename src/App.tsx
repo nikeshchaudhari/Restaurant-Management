a3a7f52f3e46715972618719e81b44f0dfd27fce
nikesh chauhdari <info.nikesh12@gmail.com>
2026-02-09 13:56:27 +0545

    rms

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,17 @@ import Home from "./pages/Home"
 import SignUp from "./pages/SignUp"
 import SignIn from "./pages/SignIn"
 import { ToastContainer } from "react-toastify"
+import AdminDashboard from "./pages/AdminDashboard"
 
 
 export const myroute=createBrowserRouter([
 {path:"",Component:Home},
 {path:"home",Component:Home},
 {path:"signup",Component:SignUp},
-{path:"login",Component:SignIn}
+{path:"login",Component:SignIn},
+{path:"admin",Component:AdminDashboard, children:[
+  
+]}
 ])
 const App = () => {
 
