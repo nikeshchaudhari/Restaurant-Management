2f52877fc3b9a9e2ff09a0e70b73c11788bfd004
nikesh chauhdari <info.nikesh12@gmail.com>
2026-02-10 21:33:20 +0545

    rms

--- src/pages/AdminDashboard.tsx
+++ src/pages/AdminDashboard.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom"
-import adminLogo from "../assets/adminlogo.png"
+// import adminLogo from "../assets/adminlogo.png"
 import { useEffect, useState } from "react"
 import axios from "axios";
 import { toast } from "react-toastify";
