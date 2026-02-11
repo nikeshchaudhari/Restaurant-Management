c8f6fcecfe9ce7e63dcd535574535dc1a77978b6
nikesh chauhdari <info.nikesh12@gmail.com>
2026-02-11 15:16:05 +0545

    rms

--- src/pages/UserAdd.tsx
+++ src/pages/UserAdd.tsx
@@ -1,7 +1,37 @@
+import React, { useState } from "react";
 import Slide from "../components/Slide";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const UserAdd = () => {
+const[fullName,Setfullname]= useState<string>("")
+const[email,SetEmail]= useState<string>("")
+const[password,SetPassword]= useState<string>("")
+const[role,SetRole]= useState<string>("waiter")
+
+const data ={
+    fullName,
+    email,
+    password,
+    role
+
+    
+}
+
+const formHandle = async (e:React.FormEvent<HTMLFormElement>)=>{
+    e.preventDefault();
+
+    try{
+    const res = await axios.post("http://localhost:3000/newuser")
+
+    }catch(err){
+
+    }
+
+    
+
+}
+
   return (
     <main className="flex">
       <Slide />
@@ -19,7 +49,7 @@ const UserAdd = () => {
 
         {/* UserAdd  */}
         <div className=" flex justify-center p-2 md:p-0 mx-5 md:mx-2 lg:mx-0">
-          <form className="bg-white w-full md:w-250 h-full mt-5 rounded-md p-5">
+          <form onSubmit={formHandle} className="bg-white w-full md:w-250 h-full mt-5 rounded-md p-5">
             <h1 className="text-2xl font-medium mb-3">Add Users</h1>
             <input
               type="text"
@@ -27,6 +57,17 @@ const UserAdd = () => {
               className="border outline-none  w-full p-2  rounded mb-3 focus:ring-1 focus:ring-blue-500"
               name="fullname"
               autoComplete="off"
+
+              value={fullName}
+              onChange={(e)=>{
+                Setfullname(e.target.value);
+                console.log(e.target.value);
+                
+              }
+              
+                
+              }
+
             />
             <input
               type="email"
@@ -34,6 +75,10 @@ const UserAdd = () => {
               className="border outline-none  w-full p-2  rounded mb-3 focus:ring-1 focus:ring-blue-500"
                name="email"
                autoComplete="off"
+               value={email}
+               onChange={(e)=>{
+                SetEmail(e.target.value)
+               }}
             />
             <div className="relative">
               <input
@@ -42,17 +87,24 @@ const UserAdd = () => {
                 className="border outline-none  w-full p-2  rounded mb-3 focus:ring-1 focus:ring-blue-500"
                 name="password"
                 autoComplete="new-password"
-              />
+                value={password}
+
+                onChange={(e)=>{
+                    SetPassword(e.target.value)
+                }}              />
             </div>
             
-            <select name="" id="" className="border outline-none  w-full p-2  rounded mb-3 focus:ring-1 focus:ring-blue-500">
+            <select name="" id="" className="border outline-none  w-full p-2  rounded mb-3 focus:ring-1 focus:ring-blue-500"
+            value={role} onChange={(e)=>{
+                SetRole(e.target.value)
+            }}>
                 <option value="">--SELECT ROLE--</option>
                 <option value="admin">Admin</option>
                 <option value="">Waiters</option>
                 
             </select>
            <div className="w-full flex justify-center">
-             <button className="w-full bg-[#080833] p-2 rounded text-white md:font-bold cursor-pointer transition hover:bg-[#232341] duration-300">
+             <button type="submit" className="w-full bg-[#080833] p-2 rounded text-white md:font-bold cursor-pointer transition hover:bg-[#232341] duration-300">
                 Add User
             </button>
            </div>
