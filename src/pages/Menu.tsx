b86a70aee0e606dc4d75f73861e87d3f0001bfd7
nikesh chauhdari <info.nikesh12@gmail.com>
2026-02-23 16:28:09 +0545

    rms

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -9,7 +9,7 @@ const Menu = () => {
         <MobileDashboard />
 
         <Slide />
-        <section className="w-screen h-full bg-[#E9E9E9] overflow-hidden">
+        <section className="w-screen bg-[#E9E9E9] overflow-hidden">
           <div className=" flex justify-between mx-5 mt-5 bg-white p-2 rounded-full items-center">
             <h1 className="mx-2 md:text-[20px] font-bold">Menu</h1>
             <Link to="/login">
@@ -20,11 +20,48 @@ const Menu = () => {
             </Link>
           </div>
           {/* Add Menu Form */}
-           <div className=" flex justify-center p-2 md:p-0 mx-5 md:mx-2 lg:mx-0">
+          <div className=" flex justify-center p-2 md:p-0 mx-5 md:mx-2 lg:mx-0">
+            <form className="bg-white w-full md:w-250 h-full mt-5 rounded-md p-5">
+              <h1 className="text-2xl font-medium mb-3">Menu Add</h1>
+              <input
+                type="text"
+                placeholder="Enter Menu Name"
+                className="border border-gray-300 outline-none  w-full p-2  rounded mb-3 focus:ring-1 focus:ring-blue-500 "
+              />
+              <input
+                type="text"
+                placeholder="Enter Price"
+                className="border border-gray-300 outline-none  w-full p-2  rounded mb-3 focus:ring-1 focus:ring-blue-500 "
+              />
+              <select
+                name=""
+                id=""
+                className="border border-gray-300 outline-none  w-full p-2  rounded mb-3 focus:ring-1 focus:ring-blue-500"
+              >
+                <option value="">--SELECT CATEGORY--</option>
+                <option value="pizza">Pizza</option>
+                <option value="burger">Burger</option>
+                <option value="desert">Desert</option>
+              </select>
+              <div className="w-full flex justify-center">
+                <button
+                  type="submit"
+                  className="w-full bg-[#080833] p-2 rounded text-white md:font-bold cursor-pointer transition hover:bg-[#232341] duration-300"
+                >
+                  Add Menu
+                </button>
+              </div>
+            </form>
+          </div>
+
+          {/* View All Menu */}
+          <div className=" md:flex justify-center  overflow-x-auto p-5">
+            <table className="bg-white min-w-50 w-full md:w-250 h-full mt-5 rounded-md ">
 
 
-            
-           </div>
+              
+            </table>
+          </div>
         </section>
       </main>
     </>
