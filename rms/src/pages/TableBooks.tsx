3c02b47941d891b8130f2e7a370ddc66a3d77e16
nikesh <info.nikesh12@gmail.com>
2026-03-19 12:53:50 +0545

    rms

--- rms/src/pages/TableBooks.tsx
+++ rms/src/pages/TableBooks.tsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from "react-router-dom";
+import {  useNavigate } from "react-router-dom";
 import MobileDashboard from "../components/MobileDashboard";
 import Slide from "../components/Slide";
 import { useEffect, useState } from "react";
