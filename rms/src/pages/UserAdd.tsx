c0a1dcfb7d8b264b7861a90b835d76cfb4afdf18
nikesh <info.nikesh12@gmail.com>
2026-03-24 16:01:55 +0545

    rms

--- rms/src/pages/UserAdd.tsx
+++ rms/src/pages/UserAdd.tsx
@@ -26,6 +26,7 @@ const UserAdd = () => {
   const [role, SetRole] = useState<string>("waiter");
   const [users, setUser] = useState<User[]>([]);
   const [editUser, setEditUser] = useState<User | null>(null);
+  const [showDelete, setShowDelete] = useState(true);
   const navigate = useNavigate();
 
   const data = {
@@ -124,8 +125,6 @@ const UserAdd = () => {
   // delete data
 
   const deleteUser = async (id: any) => {
-    if (!window.confirm("Are You Sure you want  to delete this user?")) return;
-
     try {
       const token = localStorage.getItem("token");
       if (!token) {
@@ -156,191 +155,206 @@ const UserAdd = () => {
     navigate("/login");
   };
   return (
-    <main className="flex">
-      <MobileDashboard />
-      <Slide />
-      {/* Dashboard  */}
-      <section className="w-screen  bg-[#E9E9E9] min-h-screen">
-        <div className=" flex justify-between mx-5 mt-5 bg-white p-2 rounded-full items-center">
-          <h1 className="mx-2 md:text-[20px] font-bold">
-            {editUser ? "EditUser" : "Users"}
-          </h1>
+    <>
 
-          <button
-            className="hidden md:block rounded-full bg-[#1F354D] text-[12px] md:text-[18px] w-20 md:w-30 p-2 text-white cursor-pointer transition-all  hover:bg-[#445971]  duration-300"
-            onClick={handleLogout}
-          >
-            Logout
-          </button>
-
-          <span className="md:hidden" onClick={() => dispatch(menuOpen())}>
-            {Open ? <X /> : <Menu />}
-          </span>
+   
+      <main className="flex relative ">
+       {showDelete &&(
+        <div className="bg-white/50 border w-120 h-60 rounded absolute top-1/3 right-1/3">
+         <h1 className="text-center">Delete</h1>
         </div>
+       )}
+        <MobileDashboard />
+        <Slide />
+        {/* Dashboard  */}
 
-        {/* UserAdd  */}
-        <div className="max-w-full flex justify-center p-2 md:p-0 mx-5 md:mx-2 lg:mx-0">
-          <form
-            onSubmit={formHandle}
-            className="bg-white w-full md:w-full md:mx-5 h-full mt-5 rounded-md p-5"
-          >
-            <h1 className="text-2xl font-medium mb-3">
-              {editUser ? " Update Users" : "Add Users"}
+        <section className="w-screen  bg-[#E9E9E9] min-h-screen">
+          
+          <div className=" flex justify-between mx-5 mt-5 bg-white p-2 rounded-full items-center">
+            <h1 className="mx-2 md:text-[20px] font-bold">
+              {editUser ? "EditUser" : "Users"}
             </h1>
-            <input
-              type="text"
-              placeholder="Enter Full Name"
-              className="border border-gray-300 outline-none  w-full p-2  rounded mb-3 focus:ring-1 focus:ring-blue-500 "
-              name="fullname"
-              autoComplete="off"
-              value={fullName}
-              onChange={(e) => {
-                SetfullName(e.target.value);
-              }}
-            />
-            <input
-              type="email"
-              placeholder="Enter Email"
-              className="border border-gray-300 outline-none  w-full p-2  rounded mb-3 focus:ring-1 focus:ring-blue-500"
-              name="email"
-              autoComplete="off"
-              value={email}
-              onChange={(e) => {
-                SetEmail(e.target.value);
-              }}
-            />
-            <div className="relative">
+
+            <button
+              className="hidden md:block rounded-full bg-[#1F354D] text-[12px] md:text-[18px] w-20 md:w-30 p-2 text-white cursor-pointer transition-all  hover:bg-[#445971]  duration-300"
+              onClick={handleLogout}
+            >
+              Logout
+            </button>
+
+            <span className="md:hidden" onClick={() => dispatch(menuOpen())}>
+              {Open ? <X /> : <Menu />}
+            </span>
+          </div>
+          <div>
+
+          {/*    */}
+           
+            {/* UserAdd  */}
+          <div className="max-w-full flex justify-center p-2 md:p-0 mx-5 md:mx-2 lg:mx-0">
+            <form
+              onSubmit={formHandle}
+              className="bg-white w-full md:w-full md:mx-5 h-full mt-5 rounded-md p-5"
+            >
+              <h1 className="text-2xl font-medium mb-3">
+                {editUser ? " Update Users" : "Add Users"}
+              </h1>
               <input
-                type="password"
-                placeholder="Enter Password"
-                className="border border-gray-300 outline-none  w-full p-2  rounded mb-3 focus:ring-1 focus:ring-blue-500"
-                name="password"
-                autoComplete="new-password"
-                value={password}
+                type="text"
+                placeholder="Enter Full Name"
+                className="border border-gray-300 outline-none  w-full p-2  rounded mb-3 focus:ring-1 focus:ring-blue-500 "
+                name="fullname"
+                autoComplete="off"
+                value={fullName}
                 onChange={(e) => {
-                  SetPassword(e.target.value);
+                  SetfullName(e.target.value);
                 }}
               />
-            </div>
-
-            <select
-              name=""
-              id=""
-              className="border border-gray-300 outline-none  w-full p-2  rounded mb-3 focus:ring-1 focus:ring-blue-500"
-              value={role}
-              onChange={(e) => {
-                SetRole(e.target.value);
-              }}
-            >
-              <option value="">--SELECT ROLE--</option>
-              <option value="admin">Admin</option>
-              <option value="waiter">Waiter</option>
-            </select>
-            <div className="w-full ">
-              <button
-                type="submit"
-                className=" bg-[#080833] px-6 py-2 rounded text-white  cursor-pointer transition hover:bg-[#232341] duration-300 mr-5"
-              >
-                {editUser ? "Update User" : "Add User"}
-              </button>
-
-              {editUser && (
-                <button type="button"
-                onClick={()=>{
-                  setEditUser(null);
-                  // reset field
-
-                  SetfullName("");
-                  SetEmail("");
-                  SetPassword("");
-                  SetRole("")
-
-                  toast.info("Edit Cancel")
-                  
+              <input
+                type="email"
+                placeholder="Enter Email"
+                className="border border-gray-300 outline-none  w-full p-2  rounded mb-3 focus:ring-1 focus:ring-blue-500"
+                name="email"
+                autoComplete="off"
+                value={email}
+                onChange={(e) => {
+                  SetEmail(e.target.value);
                 }}
-                
-                
-                className=" bg-[#080833] px-6 py-2  rounded text-white  cursor-pointer transition hover:bg-[#232341] duration-300">Cancel</button>
-                
-              )}
-              
-            </div>
-           
-          </form>
-        </div>
+              />
+              <div className="relative">
+                <input
+                  type="password"
+                  placeholder="Enter Password"
+                  className="border border-gray-300 outline-none  w-full p-2  rounded mb-3 focus:ring-1 focus:ring-blue-500"
+                  name="password"
+                  autoComplete="new-password"
+                  value={password}
+                  onChange={(e) => {
+                    SetPassword(e.target.value);
+                  }}
+                />
+              </div>
 
-        {/* Data View */}
-        <div className=" md:flex justify-center  overflow-x-auto p-5">
-          <table className="bg-white  w-full  h-full mt-5 rounded-md ">
-            <thead className="bg-gray-100 ">
-              <tr>
-                <th className=" px-4 py-2 text-left ">No.</th>
-                <th className=" px-4 py-2 text-left">Name</th>
-                <th className=" px-4 py-2 text-left">Role</th>
-                <th className=" px-4 py-2 text-left">Username</th>
-                <th className=" px-4 py-2 text-left">Actions</th>
-              </tr>
-            </thead>
-            <tbody>
-              {users.length === 0 ? (
-                <tr>
-                  <td
-                    colSpan={5}
-                    className="text-center p-5 text-[20px] font-bold"
+              <select
+                name=""
+                id=""
+                className="border border-gray-300 outline-none  w-full p-2  rounded mb-3 focus:ring-1 focus:ring-blue-500"
+                value={role}
+                onChange={(e) => {
+                  SetRole(e.target.value);
+                }}
+              >
+                <option value="">--SELECT ROLE--</option>
+                <option value="admin">Admin</option>
+                <option value="waiter">Waiter</option>
+              </select>
+              <div className="w-full ">
+                <button
+                  type="submit"
+                  className=" bg-[#080833] px-6 py-2 rounded text-white  cursor-pointer transition hover:bg-[#232341] duration-300 mr-5"
+                >
+                  {editUser ? "Update User" : "Add User"}
+                </button>
+
+                {editUser && (
+                  <button
+                    type="button"
+                    onClick={() => {
+                      setEditUser(null);
+                      // reset field
+
+                      SetfullName("");
+                      SetEmail("");
+                      SetPassword("");
+                      SetRole("");
+
+                      toast.info("Edit Cancel");
+                    }}
+                    className=" bg-[#080833] px-6 py-2  rounded text-white  cursor-pointer transition hover:bg-[#232341] duration-300"
                   >
-                    No Users Found
-                  </td>
+                    Cancel
+                  </button>
+                )}
+              </div>
+            </form>
+          </div>
+
+          {/* Data View */}
+          <div className=" md:flex justify-center  overflow-x-auto p-5">
+            <table className="bg-white  w-full  h-full mt-5 rounded-md ">
+              <thead className="bg-gray-100 ">
+                <tr>
+                  <th className=" px-4 py-2 text-left ">No.</th>
+                  <th className=" px-4 py-2 text-left">Name</th>
+                  <th className=" px-4 py-2 text-left">Role</th>
+                  <th className=" px-4 py-2 text-left">Username</th>
+                  <th className=" px-4 py-2 text-left">Actions</th>
                 </tr>
-              ) : (
-                users.map((user, index) => (
-                  <tr key={index} className="hover:bg-gray-400/10">
-                    <td className=" px-2 md:px-4  py-2">
-                      {users.length - index}
-                    </td>
-                    <td className=" px-2 md:px-4 py-2">{user?.fullName}</td>
-                    <td className="px-2 md:px-4 py-2">{user?.email}</td>
-                    <td className="px-2 md:px-4 py-2">{user?.role}</td>
-                    <td className="flex gap-5 justify-start items-center px-2 md:px-4 py-2 text-[20px] ">
-                      <div className="relative  group">
-                        <SquarePen
-                          className="text-[#080833] cursor-pointer transform hover:-translate-y-0.5 duration-300  "
-                          onClick={() => {
-                            setEditUser(user);
-                            console.log("Edit clicked:", user);
-
-                            SetfullName(user.fullName);
-                            console.log("Setting fullName:", user.fullName);
-                            SetEmail(user.email);
-                            SetRole(user.role);
-                          }}
-                        />
-
-                        <span
-                          className="absolute bottom-full left-1/2 hidden group-hover:block bg-black text-white text-sm mb-2 rounded px-2 py-1 whitespace-nowrap 
-                        "
-                        >
-                          Edit
-                        </span>
-                      </div>
-                      <div className="relative group">
-                        <Trash2
-                          className="text-black cursor-pointer transform hover:-translate-y-0.5 duration-300"
-                          onClick={() => deleteUser(user._id)}
-                        />
-
-                        <span className="absolute left-1/2 bottom-full bg-red-600 text-white text-sm rounded px-2 py-1 mb-2 hidden group-hover:block">
-                          Delete
-                        </span>
-                      </div>
+              </thead>
+              <tbody>
+                {users.length === 0 ? (
+                  <tr>
+                    <td
+                      colSpan={5}
+                      className="text-center p-5 text-[20px] font-bold"
+                    >
+                      No Users Found
                     </td>
                   </tr>
-                ))
-              )}
-            </tbody>
-          </table>
-        </div>
-      </section>
-    </main>
+                ) : (
+                  users.map((user, index) => (
+                    <tr key={index} className="hover:bg-gray-400/10">
+                      <td className=" px-2 md:px-4  py-2">
+                        {users.length - index}
+                      </td>
+                      <td className=" px-2 md:px-4 py-2">{user?.fullName}</td>
+                      <td className="px-2 md:px-4 py-2">{user?.email}</td>
+                      <td className="px-2 md:px-4 py-2">{user?.role}</td>
+                      <td className="flex gap-5 justify-start items-center px-2 md:px-4 py-2 text-[20px] ">
+                        <div className="relative  group">
+                          <SquarePen
+                            className="text-[#080833] cursor-pointer transform hover:-translate-y-0.5 duration-300  "
+                            onClick={() => {
+                              setEditUser(user);
+                              console.log("Edit clicked:", user);
+
+                              SetfullName(user.fullName);
+                              console.log("Setting fullName:", user.fullName);
+                              SetEmail(user.email);
+                              SetRole(user.role);
+                            }}
+                          />
+
+                          <span
+                            className="absolute bottom-full left-1/2 hidden group-hover:block bg-black text-white text-sm mb-2 rounded px-2 py-1 whitespace-nowrap 
+                        "
+                          >
+                            Edit
+                          </span>
+                        </div>
+                        <div className="relative group">
+                          <Trash2
+                            className="text-black cursor-pointer transform hover:-translate-y-0.5 duration-300"
+                            onClick={() => deleteUser(user._id)}
+                          />
+
+                          <span className="absolute left-1/2 bottom-full bg-red-600 text-white text-sm rounded px-2 py-1 mb-2 hidden group-hover:block">
+                            Delete
+                          </span>
+                        </div>
+                      </td>
+                    </tr>
+                  ))
+                )}
+              </tbody>
+            </table>
+          </div>
+          </div>
+
+          
+        </section>
+      </main>
+    </>
   );
 };
 
