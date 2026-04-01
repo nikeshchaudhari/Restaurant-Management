d3087e07ebe1dc3bf25ee10c8c40ba1858e9e87b
nikesh <info.nikesh12@gmail.com>
2026-04-01 16:31:32 +0545

    rms

--- rms/src/store/store.ts
+++ rms/src/store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { menuSlice } from "../features/menuSlice";
+import { authSlice } from "../features/Auth";
 export const store = configureStore({
 
     reducer:{
-        menu:menuSlice.reducer
+        menu:menuSlice.reducer,
+        auth:authSlice.reducer
     }
 })
 
