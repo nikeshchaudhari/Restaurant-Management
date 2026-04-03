4e03d2593c55e62c69104387716b7be56731dbec
nikesh <info.nikesh12@gmail.com>
2026-04-03 13:47:04 +0545

    rms

--- rms/src/features/Auth.ts
+++ rms/src/features/Auth.ts
@@ -1,31 +1,40 @@
-import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
 
-interface AuthState {
-  isLoggedIn: boolean;
-  user: {
-    name: string;
-    profileImage: string;
-  }| null;
+interface AuthState{
+  isLoggedIn:boolean,
+  user:{
+    name:string,
+    profileImage:string
+
+  }| null
 }
 
+
 export const authSlice = createSlice({
-  name: "user",
-  initialState: {
-    isLoggedIn: false,
-    user: null,
+  name:"auth",
+  initialState:{
+    isLoggedIn:false,
+    user:null
+
   },
-  reducers: {
-    login:(state:AuthState,action:PayloadAction<{name:string; profileImage:string}>)=>{
-        state. isLoggedIn = true;
-        state.user =action.payload
+  reducers:{
+    login:(state:AuthState,action:PayloadAction<{name:string,profileImage:string}>)=>{
+      state.isLoggedIn=false,
+      state.user=action.payload
 
     },
-    logout:(state:AuthState)=>{
-        state.isLoggedIn = false;
-        state.user = null;
-    }
-  },
-});
 
-export const{login,logout}=authSlice.actions;
-export default authSlice.reducer;
+    logout:(state:AuthState)=>{
+      state.isLoggedIn=false,
+      state.user=null
+    },
+    // updateProfileImage:(state,action:PayloadAction<string>)=>{
+    //   if(state.user){
+    //     state.user.profileImage=action.payload
+    //   }
+    // }
+  }
+})
+
+export const{login,logout}=authSlice.actions
+export default authSlice.reducer
