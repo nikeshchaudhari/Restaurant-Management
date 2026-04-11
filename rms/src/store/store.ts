7aa55238fbec6766e712d89021042affdbdf21e2
nikesh <info.nikesh12@gmail.com>
2026-04-11 23:46:15 +0545

    rms

--- rms/src/store/store.ts
+++ rms/src/store/store.ts
@@ -1,13 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { menuSlice } from "../features/menuSlice";
 import { authSlice } from "../features/Auth";
+import { cartSlice } from "../features/CartSlice";
+import { cartUi } from "../features/CartOpen";
 export const store = configureStore({
+  reducer: {
+    menu: menuSlice.reducer,
+    auth: authSlice.reducer,
+    cart: cartSlice.reducer,
+    cartUi:cartUi.reducer,
+  },
+});
 
-    reducer:{
-        menu:menuSlice.reducer,
-        auth:authSlice.reducer
-    }
-})
-
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
