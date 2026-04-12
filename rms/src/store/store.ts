e94a0ed43e4f7a90d576da044849e9da8f148cec
nikesh <info.nikesh12@gmail.com>
2026-04-12 22:10:44 +0545

    rms

--- rms/src/store/store.ts
+++ rms/src/store/store.ts
@@ -1,16 +1,33 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { menuSlice } from "../features/menuSlice";
 import { authSlice } from "../features/Auth";
 import { cartSlice } from "../features/CartSlice";
 import { cartUi } from "../features/CartOpen";
-export const store = configureStore({
-  reducer: {
-    menu: menuSlice.reducer,
+import { persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'
+import persistReducer from 'redux-persist/es/persistReducer';
+
+
+const rootReducer = combineReducers({
+   menu: menuSlice.reducer,
     auth: authSlice.reducer,
     cart: cartSlice.reducer,
     cartUi:cartUi.reducer,
-  },
+
 });
+
+const persistConfig = {
+  key:'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer:persistedReducer,
+});
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
