55317fefca2c3f97687759d32d6792fc779686bc
nikesh <info.nikesh12@gmail.com>
2026-04-13 15:04:33 +0545

    rms

--- rms/src/main.tsx
+++ rms/src/main.tsx
@@ -4,9 +4,13 @@ import './index.css'
 import App from './App.tsx'
 import {Provider} from 'react-redux';
 import { store } from './store/store.ts';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor } from './store/store';
 createRoot(document.getElementById('root')!).render(
 <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
  <App />
+ </PersistGate>
 </Provider>
    
 
