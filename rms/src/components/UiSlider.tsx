542d2cee7c227e5b894cd30045d4525592352e1b
nikesh <info.nikesh12@gmail.com>
2026-04-15 14:30:28 +0545

    rms

---
+++ rms/src/components/UiSlider.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import logo from "../assets/logo.png"
+const UiSlider = () => {
+  return (
+   <>
+   <div className=' w-full h-15 bg-white shadow flex items-center '>
+   <div className='px-5'>
+   <h2 className="font-['poppins'] text-[25px] font-bold">Hamro Restor</h2>
+   </div>
+   </div>
+   </>
+  )
+}
+
+export default UiSlider
