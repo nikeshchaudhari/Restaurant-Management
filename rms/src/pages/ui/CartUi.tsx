5519706f5c173cb1a3201de143a599c3039b2d9b
nikesh <info.nikesh12@gmail.com>
2026-04-18 17:49:09 +0545

    rms

--- rms/src/pages/ui/CartUi.tsx
+++ rms/src/pages/ui/CartUi.tsx
@@ -1,65 +1,110 @@
-
 import { useDispatch, useSelector } from "react-redux";
 import type { AppDispatch, RootState } from "../../store/store";
 import { closeCart } from "../../features/CartOpen";
-import { X } from "lucide-react";
+import { Minus, Plus, X } from "lucide-react";
+import { decreaseQty, increaseQty, removeCart } from "../../features/CartSlice";
 
 const CartUi = () => {
   const dispatch: AppDispatch = useDispatch();
   const isOpen = useSelector((state: RootState) => state.cartUi.isCartOpen);
 
-  const cart = useSelector((state:RootState)=>state.cart.items)
+  const cart = useSelector((state: RootState) => state.cart.items);
   console.log(cart);
+
+const totalPrice = cart.reduce(
+  (t,i:any) => t+i.price* i.quantity  ,
+  0
+);
+  console.log(totalPrice);
   
 
   return (
     <>
-      {/* overlay */}
+      {/* OVERLAY */}
       <div
-        className={`fixed top-0 left-0 w-screen h-full bg-black/70 z-50 ${
+        className={`fixed inset-0 bg-black/70 z-40 ${
           isOpen ? "block" : "hidden"
-        } `}
-        onClick={() => dispatch(closeCart())}
-      ></div>
-
-      {/* drawer */}
-
-      <div
-        className={`  items fixed top-0 right-0 w-[40vw] bg-white h-full z-50 transition transform duration-700 p-2 ${
-          isOpen ? "translate-x-0" : "translate-x-full"
         }`}
+        onClick={() => dispatch(closeCart())}
+      />
+
+      {/* DRAWER */}
+      <div
+        className={`fixed top-0 right-0 w-full md:w-[40vw] h-full bg-white z-50
+        flex flex-col transform transition-transform duration-500
+        ${isOpen ? "translate-x-0" : "translate-x-full"}`}
       >
-       <div className="flex justify-between px-5 py-4 border-b">
-         <h2 className=" text-[20px] font-['poppins'] font-semibold">
-          Add Items
-        </h2>
-        <X
-          className="hover:bg-gray-100  cursor-pointer transform transition hover:rotate-90 duration-200 rounded-full"
-          size={30}
-          onClick={() => dispatch(closeCart())}
-        />
-       </div>
+        {/* HEADER */}
+        <div className="flex justify-between px-5 py-4 border-b">
+          <h2 className="text-[20px] font-semibold">Add Items</h2>
 
-       {/* items list */}
+          <X
+            size={30}
+            className="cursor-pointer"
+            onClick={() => dispatch(closeCart())}
+          />
+        </div>
 
-       {cart.map((items:any)=>{
-        return(
-          <>
-          <div key={items} >
-            {items.menuName}
-          </div>
+        {/* ITEMS (SCROLL AREA) */}
+        <div className="flex-1 overflow-y-auto">
+          {cart.map((items: any, index: any) => (
+            <div
+              key={items._id}
+              className="flex justify-between items-center p-2 "
+            >
+              <div className="flex items-center gap-3">
+                <img
+                  src={items.imageUrl}
+                  className="w-20 h-20 border border-gray-300 rounded p-0.5"
+                />
+
+                <div>
+                  <div className="flex flex-col gap-2">
+                    <h2 className="font-medium">{items.menuName}</h2>
+                    <h2 className="font-['poppins']">
+                      {items.quantity} * {items.price} ={" "}
+                      <span className="text-red-900 ">
+                        {items.price * items.quantity}
+                      </span>
+                    </h2>
+                  </div>
+
+                  <div className="flex items-center gap-3 mt-2">
+                    <div
+                      className="p-1 rounded hover:bg-gray-300 cursor-pointer"
+                      onClick={() => dispatch(decreaseQty(items._id))}
+                    >
+                      <Minus size={18} />
+                    </div>
+                    <span>{items.quantity}</span>
+                    <div
+                      className="p-1 rounded hover:bg-gray-300 cursor-pointer"
+                      onClick={() => dispatch(increaseQty(items._id))}
+                    >
+                      <Plus size={18} />
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <X
+                className="cursor-pointer"
+                onClick={() => dispatch(removeCart(items._id))}
+              />
+            </div>
+          ))}
+        </div>
+
+        {/* FOOTER */}
+        <div className="border-t p-4 flex gap-2 ">
+          <h2 className="font-['poppins'] font-bold">Total Price : </h2>
+          <h2 className="font-['poppins'] text-red-900">Rs.{totalPrice}</h2>
+        </div>
+        <button className={` p-2 mx-4 md:mx-2 mb-10 md:mb-2 rounded text-white font-['poppins']  transition-all transform  text-[18px] font-medium  ${
+          cart.length === 0 ?"bg-gray-400 cursor-not-allowed":"bg-red-900 cursor-pointer hover:bg-red-800 transition-all transform hover:-translate-y-1 duration-500"
           
-          </>
-        )
-       })}
-
-       <div>
-
-       </div>
-
-
-
-      </div>
+          }`} disabled={cart.length === 0}>Order Confirm</button>
+      </div>  
     </>
   );
 };
