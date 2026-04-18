5218726726346e484408fe7bff6981c7a1f6012c
nikesh <info.nikesh12@gmail.com>
2026-04-18 17:49:20 +0545

    rms

--- rms/src/pages/ui/ListTabel.tsx
+++ rms/src/pages/ui/ListTabel.tsx
@@ -3,6 +3,11 @@ import Navbar from "../../components/Navbar";
 import UiSlider from "../../components/UiSlider";
 import { ArrowRightCircleIcon, Table } from "lucide-react";
 import axios from "axios";
+import type { AppDispatch, RootState } from "../../store/store";
+import { useDispatch, useSelector } from "react-redux";
+import { menuOpen } from "../../features/menuSlice";
+import { useNavigate } from "react-router-dom";
+import { setSelectedTable } from "../../features/TableSlice";
 
 interface Table {
   _id: string;
@@ -12,8 +17,14 @@ interface Table {
 }
 const ListTabel = () => {
   const [tables, setTables] = useState<Table[]>([]);
-  const [selectedTable, setSelectedTable] = useState<Table[]>([]);
+  const [selected, setSelected] = useState<Table | null>(null);
 
+  const dispatch: AppDispatch = useDispatch();
+  const open = useSelector((state: RootState) => state.menu.isOpen);
+
+  const navigate = useNavigate();
+
+  // Table data fetch
   useEffect(() => {
     const fetchTable = async () => {
       const res = await axios.get("http://localhost:3000/table/all-table");
@@ -23,10 +34,20 @@ const ListTabel = () => {
     fetchTable();
   }, []);
 
-  const handleTable = (table: any) => {
-    setSelectedTable(table);
-    console.log("table", setSelectedTable(table));
+  // selected Table
+  const selectTable = (tables: Table) => {
+    setSelected(tables);
   };
+
+  // cartopen
+
+  const handleMenu = () => {
+    if (!selected) return;
+
+    dispatch(setSelectedTable(selected));
+    navigate("/food-order/all-menu");
+  };
+
   return (
     <>
       <main>
@@ -49,9 +70,9 @@ const ListTabel = () => {
                 <div className="bg-amber-500/40 px-2 py-1 md:px-5 md:py-2 rounded-full text-white transition transform hover:-translate-x-1 duration-300 font-['poppins']">
                   <h2 className="text-[12px] md:text-[18px]">Unavailable</h2>
                 </div>
-                {/* <div className="bg-black/40 px-5 py-2 rounded-full text-white transition transform hover:-translate-x-1 duration-300 font-['poppins']">
+                <div className="bg-[#360404] px-5 py-2 rounded-full text-white transition transform hover:-translate-x-1 duration-300 font-['poppins']">
                   <h2 className="">Selected</h2>
-                </div> */}
+                </div>
               </div>
             </div>
 
@@ -61,24 +82,32 @@ const ListTabel = () => {
               <h2 className="text-[18px] pl-10 pt-5 font-semibold font-['poppins']">
                 Tables
               </h2>
-              <div className="grid grid-cols-4 md:grid-cols-5 lg:grid-cols-8 gap-5 p-5 justify-items-center 0">
+              <div className="grid grid-cols-4 md:grid-cols-5 lg:grid-cols-9 gap-5 p-5 justify-items-center 0">
                 {tables.map((t: any) => (
                   <button
                     disabled={t.status === "unavailable"}
+                    onClick={() => selectTable(t)}
                     key={t._id}
-                    className={`px-4 py-2 md:px-8 md:py-5 lg:px-10 lg:py-5  rounded  text-white font-['poppins'] ${
-                      t.status === "unavailable"
-                        ? "bg-amber-500/40 cursor-not-allowed"
-                        : "hover:bg-[#360404]  bg-amber-500 cursor-pointer transition duration-300 transform hove:scale-3d"
+                    className={`px-4 py-2 md:px-8 md:py-5 lg:px-10 lg:py-5  rounded  text-white font-['poppins'] text-[14px] ${
+                      selected?._id === t._id
+                        ? "bg-[#360404] scale-105 shadow-lg"
+                        : t.status === "unavailable"
+                          ? "bg-amber-500/40 cursor-not-allowed"
+                          : "hover:bg-[#360404]  bg-amber-500 cursor-pointer transition duration-300 transform hove:scale-3d"
                     }`}
                   >
                     {t.tableNumber}
                   </button>
                 ))}
               </div>
-              <div className="absolute bottom-5 right-10  hover:bg-gray-200 p-1 rounded-full cursor-pointer transform hover:translate-x-1.5 transition duration-300 ">
-                <ArrowRightCircleIcon className="text-amber-500" size={40} />
-              </div>
+              {selected && (
+                <div
+                  className="absolute bottom-5 right-10  hover:bg-gray-200 p-1 rounded-full cursor-pointer transform hover:translate-x-1.5 transition duration-300 "
+                  onClick={handleMenu}
+                >
+                  <ArrowRightCircleIcon className="text-amber-500" size={40} />
+                </div>
+              )}
             </div>
           </section>
         </div>
