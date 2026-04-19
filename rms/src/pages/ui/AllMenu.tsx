1c5ab17566be889f3a5d1345cf4f778a138e4395
nikesh <info.nikesh12@gmail.com>
2026-04-20 01:01:34 +0545

    rms

--- rms/src/pages/ui/AllMenu.tsx
+++ rms/src/pages/ui/AllMenu.tsx
@@ -4,9 +4,10 @@ import UiSlider from "../../components/UiSlider";
 import type { AppDispatch, RootState } from "../../store/store";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { addToCart, cartSlice } from "../../features/CartSlice";
+import { addToCart } from "../../features/CartSlice";
 import { openCart } from "../../features/CartOpen";
 import CartUi from "./CartUi";
+import { Bold, ChevronLeft, ChevronRight, CircleArrowLeft, CircleArrowRight, MoveRight, Underline } from "lucide-react";
 
 interface MenuItems {
   menuName: string;
@@ -24,10 +25,16 @@ interface MenuItems {
 const AllMenu = () => {
   const [menu, setMenu] = useState<MenuItems[]>([]);
   const [category, setCategory] = useState<string[]>([]);
+  const [selectCategory, setSelectCategory] = useState("All");
+  const [search, setSearch] = useState("");
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [rowPage, setRowPage] = useState(15);
+
   const table: any = useSelector(
     (state: RootState) => state.table.selectedTable,
   );
-  console.log(table);
+  // console.log(table);
 
   const dispatch: AppDispatch = useDispatch();
 
@@ -38,7 +45,7 @@ const AllMenu = () => {
       const res = await axios.get("http://localhost:3000/menu/all-menu");
 
       const menuData: MenuItems[] = res.data.allMenu;
-      setMenu(res.data.allMenu);
+      setMenu([...menuData].reverse());
 
       const uniqueCategory = [
         "All",
@@ -56,14 +63,44 @@ const AllMenu = () => {
     dispatch(openCart(item));
   };
 
+  // filterMenu
+
+  const filterMenu = menu
+    .filter((items) =>
+      selectCategory === "All" ? true : items.category === selectCategory,
+    )
+    .filter((items) =>
+      items.menuName.toLocaleLowerCase().includes(search.toLocaleLowerCase()),
+    );
+
+  const handleCategory = (items: any) => {
+    // console.log(items);
+    setSelectCategory(items);
+  };
+
+  // pagination
+
+  // const indexOfLasItems = currentPage * rowPage;
+  // const indexOfFirstItems = indexOfLasItems - rowPage;
+  // const currentItems = filterMenu.slice(indexOfFirstItems,indexOfLasItems)
+  // console.log(currentItems);
+
+  // const totalPage = Math.ceil(filterMenu.length/rowPage)
+
+  const indexOfLastPage = currentPage * rowPage;
+  const indexOfFirstPage = indexOfLastPage - rowPage;
+  const currentItems = filterMenu.slice(indexOfFirstPage, indexOfLastPage);
+  const totalPage = Math.ceil(filterMenu.length / rowPage);
+  console.log(indexOfLastPage, indexOfFirstPage, totalPage, currentItems);
+
   return (
     <>
       <main className="">
-        <Navbar />
+        <Navbar search={search} setSearch={setSearch} />
         <div className="md:flex ">
           <UiSlider />
 
-          <section className="px-5 overflow-hidden ">
+          <section className="px-5 overflow-hidden  relative ">
             <div>
               <h2 className=" py-5  lg:text-[22px] font-['poppins'] font-semibold">
                 Step 2: Take Order
@@ -75,17 +112,35 @@ const AllMenu = () => {
                 {category.map((items, index) => (
                   <button
                     key={index}
-                    className={`border border-amber-600 px-5 py-1 rounded-full cursor-pointer hover:bg-red-800 hover:text-white transition duration-300 font-['poppins']`}
+                    className={`border border-amber-600 px-5 py-1 rounded-full cursor-pointer hover:bg-red-800 hover:text-white transition duration-300 font-['poppins'] ${
+                      selectCategory === items
+                        ? "bg-red-800 text-white"
+                        : "bg-white text-black hover:bg-red-100"
+                    }`}
+                    onClick={() => handleCategory(items)}
                   >
                     {items}
                   </button>
                 ))}
               </div>
             </div>
-            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-5">
-              {menu.map((items, index) => {
-                return (
-                  <>
+            <div className="w-full mb-20">
+              {filterMenu.length === 0 ? (
+                <div className="w-full  flex flex-col justify-center items-center h-80">
+                  <h2 className="text-[20px]">No items Founds</h2>
+                  <button
+                    className="bg-red-800 w-20 h-10 m-2 rounded-sm hover:bg-red-700 text-white cursor-pointer"
+                    onClick={() => {
+                      setSearch("");
+                      setSelectCategory("All");
+                    }}
+                  >
+                    Refresh
+                  </button>
+                </div>
+              ) : (
+                <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-5">
+                  {currentItems.map((items, index) => (
                     <div
                       key={index}
                       className="w-full h-56 md:h-60  shadow-2xl rounded-2xl flex justify-center relative cursor-pointer group "
@@ -121,9 +176,27 @@ const AllMenu = () => {
                         </div>
                       </div>
                     </div>
-                  </>
-                );
-              })}
+                  ))}
+
+                  {/* pagination */}
+
+                  <div className="absolute w-full  bottom-0 z-50 flex items-center justify-end gap-5 mb-5">
+                    <div className={` rounded-full transition duration-500 ${
+                    currentPage === 1 ?" text-black/10 cursor-not-allowed":" hover:bg-red-800 cursor-pointer hover:text-white "
+                    } ` } 
+                    
+                    >
+                      <ChevronLeft size={35} onClick={()=>setCurrentPage((prev)=>Math.max(prev-1,1))}  />
+                    </div>
+                    <span className="text-[20px] font-['poppins']">{currentPage} ....... {totalPage}</span>
+                   <div className={`mr-10 rounded-full transition duration-500 ${
+                    currentPage === totalPage ? " text-black/10 cursor-not-allowed":" hover:bg-red-800 cursor-pointer hover:text-white "
+                    }`}>
+                     <ChevronRight size={35} onClick={()=>setCurrentPage((next)=>Math.min(next+1, totalPage))}/>
+                   </div>
+                  </div>
+                </div>
+              )}
             </div>
             <CartUi />
           </section>
