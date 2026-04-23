cffb142280e67e2b7e0a973ecdc73b463441a5bc
nikesh <info.nikesh12@gmail.com>
2026-04-23 23:24:29 +0545

    rms

--- rms/src/components/UiSlider.tsx
+++ rms/src/components/UiSlider.tsx
@@ -1,12 +1,21 @@
-import React, { useState } from "react";
-import logo from "../assets/logo.png";
+
+import { useLocation, useNavigate } from "react-router-dom";
 const UiSlider = () => {
-  const buttons = ["Select Table", "Take Table", "My Orders"];
-  const [active, setActive] = useState<null>(null);
+  const buttons = [
+    {name:"Select Table",path:"/food-order"},
+    {name:"Take Order",path:"/food-order/all-menu"},
+    {name:"My Orders",path:"/food-order/all-order"},
+    {name:"Profile",path:"/food-order/profile"},
+    
+    
+  ];
+  // const [active, setActive] = useState<null>(null);
+  const navigate = useNavigate()
+  const location = useLocation()
   return (
     <>
-      <div >
-        <aside className="hidden md:block md:w-[15vw] lg:w-[16vw] h-screen border-r border-gray-300 p-4 ">
+      <div className="flex min-h-screen" >
+        <aside className="hidden md:block md:w-[15vw] lg:w-[16vw] border-r min-h-full border-gray-300 p-4 ">
           <div>
             <h3 className="text-center md:p-5 md:text-[12px] lg:text-[15px] font-['poppins'] text-gray-400">
               Waiter Menu
@@ -18,14 +27,19 @@ const UiSlider = () => {
                 <div>
                   <li
                     key={index}
-                    onClick={() => setActive(index)}
+                    
+                    onClick={() => {
+                      // setActive(index);
+                      navigate(item.path)
+                      
+                    }}
                     className={`lg:py-2 bg-[#faefee] hover:bg-[#e0e0e0]/30 w-full rounded  cursor-pointer md:mb-3 lg:mb-1 font-['poppins'] md:text-[12px] lg:text-[16px] lg:pl-10 font-semibold ${
-                      active === index
+                     location.pathname === item.path
                         ? "bg-[#eb9e95] text-red-800"
                         : "bg-white"
-                    }`}
+                    }`} 
                   >
-                    {item}
+                    {item.name}
                   </li>
                 </div>
               ))}
