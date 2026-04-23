65e02ce8526c3bee555214c3151745b67a2ef349
nikesh <info.nikesh12@gmail.com>
2026-04-24 02:01:37 +0545

    rms

--- rms/src/components/UiSlider.tsx
+++ rms/src/components/UiSlider.tsx
@@ -1,11 +1,13 @@
 
+import { ChefHat, LogOut, SendToBack, Table, TableOfContents, UserRoundPen } from "lucide-react";
 import { useLocation, useNavigate } from "react-router-dom";
 const UiSlider = () => {
   const buttons = [
-    {name:"Select Table",path:"/food-order"},
-    {name:"Take Order",path:"/food-order/all-menu"},
-    {name:"My Orders",path:"/food-order/all-order"},
-    {name:"Profile",path:"/food-order/profile"},
+    {name:"Select Table",path:"/food-order",icon:<TableOfContents size={18}/>},
+    {name:"Take Order",path:"/food-order/all-menu",icon:<ChefHat size={18}/>},
+    {name:"My Orders",path:"/food-order/all-order",icon:<SendToBack size={18}/>},
+    {name:"Profile",path:"/food-order/profile",icon:<UserRoundPen size={18}/>},
+    {name:"Logout",path:"/food-order",icon:<LogOut size={18}/>},
     
     
   ];
@@ -14,8 +16,8 @@ const UiSlider = () => {
   const location = useLocation()
   return (
     <>
-      <div className="flex min-h-screen" >
-        <aside className="hidden md:block md:w-[15vw] lg:w-[16vw] border-r min-h-full border-gray-300 p-4 ">
+      <div className=" sticky top-0 h-screen " >
+        <aside className="hidden md:block md:w-[15vw] lg:w-[16vw] sticky top-10 h-[calc(100vh-4rem)] border-r border-gray-300 p-4 shadow bg-white">
           <div>
             <h3 className="text-center md:p-5 md:text-[12px] lg:text-[15px] font-['poppins'] text-gray-400">
               Waiter Menu
@@ -33,13 +35,14 @@ const UiSlider = () => {
                       navigate(item.path)
                       
                     }}
-                    className={`lg:py-2 bg-[#faefee] hover:bg-[#e0e0e0]/30 w-full rounded  cursor-pointer md:mb-3 lg:mb-1 font-['poppins'] md:text-[12px] lg:text-[16px] lg:pl-10 font-semibold ${
+                    className={` flex  items-center  gap-2 lg:py-2 bg-[#faefee] hover:bg-[#e0e0e0]/30 w-full rounded  cursor-pointer md:mb-3 lg:mb-1 font-['poppins'] md:text-[12px] lg:text-[16px]  font-semibold ${
                      location.pathname === item.path
                         ? "bg-[#eb9e95] text-red-800"
                         : "bg-white"
                     }`} 
                   >
-                    {item.name}
+                    {item.icon}
+                   <span> {item.name}</span>
                   </li>
                 </div>
               ))}
