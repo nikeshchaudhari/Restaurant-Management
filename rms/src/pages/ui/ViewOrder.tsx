10bd4b0cf54a8003d210b60197e42250e79a2646
nikesh <info.nikesh12@gmail.com>
2026-04-25 00:27:27 +0545

    rms

--- rms/src/pages/ui/ViewOrder.tsx
+++ rms/src/pages/ui/ViewOrder.tsx
@@ -1,10 +1,9 @@
-import React, { useEffect, useState } from "react";
+import  { useEffect, useState } from "react";
 import Navbar from "../../components/Navbar";
 import UiSlider from "../../components/UiSlider";
 import CartUi from "./CartUi";
 import axios from "axios";
-import Order from "../Order";
-import type { JSX } from "react/jsx-runtime";
+
 import { toast } from "react-toastify";
 import OrderSlide from "../../components/OrderSlide";
 import type { AppDispatch } from "../../store/store";
@@ -21,6 +20,7 @@ const ViewOrder = () => {
   }
 
   interface Order {
+    status: string | number | readonly string[] | undefined;
     _id: string;
     orderId: string;
     tableNumber: string;
@@ -84,14 +84,14 @@ const ViewOrder = () => {
 
   {/* Main Content */}
   <div className="w-full flex justify-center px-3 md:px-5 py-20">
-    <div className="shadow rounded w-full max-w-[1200px] bg-white p-3 md:p-5">
+    <div className="shadow rounded w-full max-w-300 bg-white p-3 md:p-5">
       <h2 className="text-[18px] md:text-[22px] font-['poppins'] font-semibold">
         My Orders
       </h2>
 
       {/* Mobile scroll */}
       <div className="overflow-x-auto mt-5 rounded">
-        <table className="min-w-[900px] md:min-w-full ">
+        <table className="min-w-225 md:min-w-full ">
           <thead className="bg-gray-100">
             <tr>
               <th className="px-4 py-3 text-left">Order Id</th>
