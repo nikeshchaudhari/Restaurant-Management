89edc89949af07746465fe3dfd2e7d7baa410068
nikesh <info.nikesh12@gmail.com>
2026-04-25 16:31:25 +0545

    rms

--- rms/src/pages/SignIn.tsx
+++ rms/src/pages/SignIn.tsx
@@ -1,136 +1,162 @@
-import { useState } from "react";
-import food from "../assets/foods.png";
-import { FiEye } from "react-icons/fi";
-import { FiEyeOff } from "react-icons/fi";
-import { Link, useNavigate } from "react-router-dom";
-import axios from "axios";
-import { toast } from "react-toastify";
-// import { AppDispatch, type RootState } from '../store/store';
-import { useDispatch, useSelector } from "react-redux";
-import { jwtDecode } from "jwt-decode";
-import { login } from "../features/Auth";
-import type { AppDispatch } from "../store/store";
-const SignIn = () => {
-  const [passwordshow, SetpasswordShow] = useState(false);
-  const [email, SetEmail] = useState<string>("");
-  const [password, SetPassword] = useState<string>("");
-
+  import { useState } from "react";
+  import food from "../assets/foods.png";
+  import { FiEye } from "react-icons/fi";
+  import { FiEyeOff } from "react-icons/fi";
+  import { Link, useNavigate } from "react-router-dom";
+  import axios from "axios";
+  import { toast } from "react-toastify";
+  // import { AppDispatch, type RootState } from '../store/store';
+  import { useDispatch, useSelector } from "react-redux";
+  import { jwtDecode } from "jwt-decode";
+  import { login } from "../features/Auth";
+  import type { AppDispatch } from "../store/store";
+  import { useFormik } from "formik";
+  import {Login} from "../schemas/LoginSchema"
+  const initialValues = {
+    email: "",
+    password: "",
+  };
+  const SignIn = () => {
+    const [passwordshow, SetpasswordShow] = useState(false);
+    // const [email, SetEmail] = useState<string>("");
+    // const [password, SetPassword] = useState<string>("");
   const dispatch: AppDispatch = useDispatch();
-  // const auth = useSelector((state:RootState)=>state.auth.isLoggedIn)
+      const navigate = useNavigate();
 
-  const navigate = useNavigate();
+    const { values, errors, resetForm, touched, handleBlur, handleChange, handleSubmit } =
+      useFormik({
+        initialValues: initialValues,
+        validationSchema: Login,
+        onSubmit: async (values) => {
 
-  const loginForm = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+          console.log(values);
+          
+          try {
+          
+            const response = await axios.post(
+              "http://localhost:3000/user/login",values
+              
+            );
 
-    try {
-      const response = await axios.post("http://localhost:3000/user/login", {
-        email,
-        password,
+            // console.log(response.data);
+
+            const data = response.data;
+
+            if (data.token) {
+              const token = data.token;
+              localStorage.setItem("token", token);
+              localStorage.setItem("role", data.role);
+              localStorage.setItem("FullName", data.fullName);
+              localStorage.setItem("Image", data.imageUrl);
+            
+              dispatch(
+                login({
+                  token:data.token,
+                  fullName: data.fullName,
+                  profileImage: data.imageUrl,
+                  role: data.role,
+                }),
+              );
+
+              toast.success("Login Sucessfully..");
+
+              if (data.role === "admin") {
+                navigate("/dashboard");
+              } else if (data.role === "waiter") {
+                navigate("/food-order");
+              } else {
+                navigate("/");
+              }
+              resetForm();
+            }else{
+              toast.error("Invalid credentials");
+            }
+          } catch (err) {
+            toast.error("Failed to login");
+          }
+        },
       });
 
-      console.log(response.data);
+  
+    // const auth = useSelector((state:RootState)=>state.auth.isLoggedIn)
 
-      const data = response.data;
 
-      if (data.token) {
-        const token = data.token;
-        localStorage.setItem("token", token);
-        localStorage.setItem("role", data.role);
-        localStorage.setItem("FullName", data.fullName);
-        localStorage.setItem("Image", data.imageUrl);
-        // localStorage.setItem("ImageUrl", response.data.imageUrl);
+    // const loginForm = async (e: React.FormEvent<HTMLFormElement>) => {
+    //   e.preventDefault();
+    // };
+    return (
+      <>
+        <div className="w-screen h-screen flex justify-center items-center bg-gray-100">
+          <div className="w-full md:w-200 h-100 rounded-lg flex overflow-hidden bg-white justify-center">
+            {/* Image section */}
+            <div className="w-1/2 bg-[#e2dddd] hidden md:block">
+              <img src={food} alt="food" className="w-100 h-100 p-10" />
+            </div>
 
-        // const payload = JSON.parse(atob(token.split(".")[1]));
-        // console.log("Payload", payload);
-        // console.log("Data0", response);
-        // console.log(token);
-        dispatch(
-          login({
-            token,
-            fullName: data.fullName,
-            profileImage: data.imageUrl,
-            role: data.role,
-          }),
-        );
+            {/* Form section */}
+            <div className=" w-full md:w-1/2  p-8 flex flex-col justify-center">
+              <form onSubmit={handleSubmit} className="flex flex-col gap-1">
+                <h1 className="text-center text-[20px] mb-3 font-bold">
+                  Login with Your account ?
+                </h1>
 
-        toast.success("Login Sucessfully..");
-
-        if (response.data.role === "admin") {
-          navigate("/dashboard");
-        } else if (response.data.role === "waiter") {
-          navigate("/food-order");
-        } else {
-          navigate("/")
-        }
-      }
-    } catch (err) {
-      toast.error("Failed to login");
-    }
-  };
-  return (
-    <>
-      <div className="w-screen h-screen flex justify-center items-center bg-gray-100">
-        <div className="w-full md:w-200 h-100 rounded-lg flex overflow-hidden bg-white justify-center">
-          {/* Image section */}
-          <div className="w-1/2 bg-[#e2dddd] hidden md:block">
-            <img src={food} alt="food" className="w-100 h-100 p-10" />
-          </div>
-
-          {/* Form section */}
-          <div className=" w-full md:w-1/2  p-8 flex flex-col justify-center">
-            <form onSubmit={loginForm} className="flex flex-col gap-1">
-              <h1 className="text-center text-[20px] mb-3 font-bold">
-                Login with Your account ?
-              </h1>
-
-              {/* <label htmlFor="email">Email<span className="text-red-500">*</span></label> */}
-              <input
-                type="email"
-                id="email"
-                className="p-2 border outline-none border-[#e2dddd] rounded mb-3 focus:ring-1 focus:ring-blue-500"
-                placeholder="Enter your email"
-                name="email"
-                value={email}
-                onChange={(e) => SetEmail(e.target.value)}
-              />
-              <div className="relative">
-                {/* <label htmlFor="password">Password<span className="text-red-500">*</span></label> */}
+               <div className="relative mb-3">
+                 {/* <label htmlFor="email">Email<span className="text-red-500">*</span></label> */}
                 <input
-                  type={passwordshow ? "text" : "password"}
-                  id="password"
-                  className="w-full p-2 border outline-none border-[#e2dddd] rounded mb-3 focus:ring-1 focus:ring-blue-500 "
-                  name="password"
-                  autoComplete="new-password"
-                  placeholder="Enter password"
-                  value={password}
-                  onChange={(e) => SetPassword(e.target.value)}
+                  type="email"
+                  id="email"
+                  className="w-full p-2 border outline-none border-[#e2dddd] rounded  focus:ring-1 focus:ring-blue-500"
+                  placeholder="Enter your email"
+                  name="email"
+                  value={values.email}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
                 />
+              {touched.email && errors.email &&(
+                <p className=" text-red-500 text-sm ">{errors.email}*</p>
+              )}
+               </div>
+                <div className="relative">
+                  {/* <label htmlFor="password">Password<span className="text-red-500">*</span></label> */}
+                  <input
+                    type={passwordshow ? "text" : "password"}
+                    id="password"
+                    className="w-full p-2 border outline-none border-[#e2dddd] rounded  focus:ring-1 focus:ring-blue-500 "
+                    name="password"
+                    autoComplete="new-password"
+                    placeholder="Enter password"
+                    value={values.password}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                  />
+                  {touched.password &&  errors.password &&(
+                    <p className="text-red-500 text-sm">{errors.password} *</p>
+                  )}
+
+                  <button
+                    type="button"
+                    className="absolute top-3 right-2 cursor-pointer "
+                    onClick={() => SetpasswordShow(!passwordshow)}
+                  >
+                    {passwordshow ? <FiEye size={18} /> : <FiEyeOff size={18} />}
+                  </button>
+                </div>
 
                 <button
-                  type="button"
-                  className="absolute top-3 right-2 cursor-pointer "
-                  onClick={() => SetpasswordShow(!passwordshow)}
+                  type="submit"
+                  className="bg-[#1992DE] p-2 mt-2 transition duration-300 hover:bg-[#0E6BA6] cursor-pointer text-white rounded"
                 >
-                  {passwordshow ? <FiEye size={18} /> : <FiEyeOff size={18} />}
+                  SignUp
                 </button>
-              </div>
-
-              <button
-                type="submit"
-                className="bg-[#1992DE] p-2 -mt-2 transition duration-300 hover:bg-[#0E6BA6] cursor-pointer text-white rounded"
-              >
-                SignUp
-              </button>
-              <span className="text-center">
-                Create Your Account? <Link to="/signup">SignUp</Link>
-              </span>
-            </form>
+                <span className="text-center">
+                  Create Your Account? <Link to="/signup">SignUp</Link>
+                </span>
+              </form>
+            </div>
           </div>
         </div>
-      </div>
-    </>
-  );
-};
+      </>
+    );
+  };
 
-export default SignIn;
+  export default SignIn;
