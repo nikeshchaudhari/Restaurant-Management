639f1a38ae82e2286b8a74b7ce394a2b087c7c4f
nikesh <info.nikesh12@gmail.com>
2026-05-01 15:40:54 +0545

    rms

---
+++ rms/src/pages/Footer.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import facebook from "../assets/icons8-facebook.gif";
+import instagram from "../assets/icons8-instagram.gif";
+const Footer = () => {
+  return (
+    <>
+      <section>
+        <div className="w-full bg-gray-100 h-full mt-10">
+          <div className="flex items-center justify-center h-14 px-3 font-['poppins']">
+            <div>
+              <span className="px-2" >© Copyright 2026, All Rights Reserved by</span>
+              <Link to="https://endcodesnepal.com/" className="font-medium text-red-800">
+                Endcodes Nepal Pvt.Ltd.
+              </Link>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Footer;
